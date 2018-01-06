138c3a9c3ed0700dbc1a7110ca1702f4ad4761b8
ihor.verkhohliad <ihor.verkhohliad@redpointpositioning.com>
2018-01-06 15:06:06 +0000

    add store, logger for store

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import Vuex from 'vuex'
 
 import VueYouTubeEmbed from 'vue-youtube-embed'
 import App from './App';
 import router from './router'
+import store from './stores/configureStore';
 
 
-Vue.use(Vuex);
 Vue.use(Vuetify);
 Vue.use(VueYouTubeEmbed);
 Vue.config.productionTip = false;
@@ -16,6 +15,7 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App, VueYouTubeEmbed }
 });
