34f9a08f3e6049734feca683af2dbf1bfcc55b0b
ihorek7777 <ihor.verkhohliad@nure.ua>
2018-02-02 15:28:37 +0200

    remove localization for a time

--- src/main.js
+++ src/main.js
@@ -2,19 +2,13 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import BootstrapVue from 'bootstrap-vue'
 import VueYouTubeEmbed from 'vue-youtube-embed'
-import VueI18n from 'vue-i18n'
-
-var Lang = require('vuejs-localization');
-Lang.requireAll(require.context('./lang', true, /\.js$/));
 
 import router from './router'
 import App from './App'
 import store from './stores/configureStore'
 
-Vue.use(Lang);
 Vue.use(BootstrapVue);
 Vue.use(Vuetify);
-Vue.use(VueI18n);
 Vue.use(VueYouTubeEmbed);
 Vue.config.productionTip = false;
 
@@ -23,5 +17,5 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: {App, VueYouTubeEmbed}
+  components: { App, VueYouTubeEmbed }
 });
