d8f319958f39c99c326c68df536a5fc394271f79
levtsiunchyk <lev.tsiunchyk@nure.ua>
2018-02-08 15:56:08 +0200

    Fix design bugs

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <v-app id="app">
-    <h1>Сервер на даний час оновлюється. Просимо вибачення за незручності.</h1>
-    <!--<div v-if="loading">-->
-      <!--<MainLoadingComponent />-->
-    <!--</div>-->
-    <!--<Header />-->
-    <!--<router-view />-->
-    <!--<Footer />-->
+    <!--<h1>Сервер на даний час оновлюється. Просимо вибачення за незручності.</h1>-->
+    <div v-if="loading">
+      <MainLoadingComponent />
+    </div>
+    <Header />
+    <router-view />
+    <Footer />
   </v-app>
 </template>
 
@@ -38,7 +38,4 @@
 
 <style>
 
-  h1 {
-    margin-top: 150px;
-  }
 </style>
