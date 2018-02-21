7f352f326fee49f2640239ea2c36c3b4456e9a06
ihorek7777 <ihor.verkhohliad@nure.ua>
2018-02-21 23:42:13 +0200

    formalize specializations entities, decrease time of http mocks

---
+++ src/pages/specialities/SpecialitiesTable.vue
@@ -0,0 +1,22 @@
+<script>
+
+  export default {
+    name: 'SpecialitiesTable',
+    data() {
+      return {
+      }
+    },
+    methods: {
+    },
+  }
+</script>
+
+<template>
+  <div class="SpecialitiesTable">
+
+  </div>
+</template>
+
+<style scoped>
+</style>
+
